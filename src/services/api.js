c1d61694ba7ad6b85dc37a5394d6434b408abfc6
Ronit-bawankule <ronitbawankule20@gmail.com>
2026-05-26 21:08:35 +0530

    Connected frontend to live backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://hospital-management-backend-u64d.onrender.com";
 
 export const getPatients = async () => {
 
