219b1db7ff493273f5e9f10a998f81192bb4c4c5
Ronit-bawankule <ronitbawankule20@gmail.com>
2026-05-26 21:22:59 +0530

    Connected frontend to deployed backend

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -13,7 +13,7 @@ function Login() {
         try {
 
             const response = await axios.post(
-                "http://localhost:8080/auth/login",
+                "https://hospital-management-backend-u64d.onrender.com/auth/login",
                 {
                     username,
                     password
