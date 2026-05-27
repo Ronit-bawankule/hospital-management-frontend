afc69028d9aa31d5d1b1ece1a730ea0e8d8f1e8a
Ronit-bawankule <ronitbawankule20@gmail.com>
2026-05-27 11:30:05 +0530

    Stable clean routing setup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    Navigate
+} from "react-router-dom";
+
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+
 function App() {
+
+    const user = JSON.parse(
+        localStorage.getItem("user")
+    );
 
     return (
 
-        <div
-            style={{
-                padding: "50px",
-                fontSize: "40px"
-            }}
-        >
+        <BrowserRouter>
 
-            APP WORKING 🚀
+            <Routes>
 
-        </div>
+                <Route
+                    path="/login"
+                    element={<Login />}
+                />
+
+                <Route
+                    path="/"
+                    element={
+                        user
+                        ? <Dashboard />
+                        : <Navigate to="/login" />
+                    }
+                />
+
+            </Routes>
+
+        </BrowserRouter>
     );
 }
 
