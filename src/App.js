d58dad083a17e1d2f07f17dbbb7bd066f68a3a07
Ronit-bawankule <ronitbawankule20@gmail.com>
2026-05-27 11:26:54 +0530

    Absolute minimal app test

--- src/App.js
+++ src/App.js
@@ -1,51 +1,17 @@
-import {
-    BrowserRouter,
-    Routes,
-    Route
-} from "react-router-dom";
-
-import Dashboard from "./pages/Dashboard";
-import HospitalForm from "./pages/HospitalForm";
-import PatientApp from "./pages/PatientApp";
-import Appointments from "./pages/Appointments";
-import Login from "./pages/Login";
-
 function App() {
 
     return (
 
-        <BrowserRouter>
+        <div
+            style={{
+                padding: "50px",
+                fontSize: "40px"
+            }}
+        >
 
-            <Routes>
+            APP WORKING 🚀
 
-                <Route
-                    path="/login"
-                    element={<Login />}
-                />
-
-                <Route
-                    path="/"
-                    element={<Dashboard />}
-                />
-
-                <Route
-                    path="/add-patient"
-                    element={<HospitalForm />}
-                />
-
-                <Route
-                    path="/patients"
-                    element={<PatientApp />}
-                />
-
-                <Route
-                    path="/appointments"
-                    element={<Appointments />}
-                />
-
-            </Routes>
-
-        </BrowserRouter>
+        </div>
     );
 }
 
