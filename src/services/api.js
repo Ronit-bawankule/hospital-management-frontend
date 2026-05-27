343f40710b803d81329b5a2a302e7dec13c3c634
Ronit Bawankule <150640545+Ronit-bawankule@users.noreply.github.com>
2026-05-27 12:37:13 +0530

    Add CRUD operations for patients in api.js

--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,43 @@ import axios from "axios";
 const BASE_URL =
 "https://hospital-management-backend-u64d.onrender.com";
 
+export const getPatients = async () => {
+
+    return await axios.get(
+        `${BASE_URL}/patients`
+    );
+};
+
+export const addPatient = async (
+    patient
+) => {
+
+    return await axios.post(
+        `${BASE_URL}/patients`,
+        patient
+    );
+};
+
+export const updatePatient = async (
+    id,
+    patient
+) => {
+
+    return await axios.put(
+        `${BASE_URL}/patients/${id}`,
+        patient
+    );
+};
+
+export const deletePatient = async (
+    id
+) => {
+
+    return await axios.delete(
+        `${BASE_URL}/patients/${id}`
+    );
+};
+
 export const getDoctors = async () => {
 
     return await axios.get(
