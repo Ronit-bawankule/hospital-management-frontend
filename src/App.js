06e8dc5963d0352f2435a2da67c907c2c60a7ef7
Ronit-bawankule <ronitbawankule20@gmail.com>
2026-05-27 10:42:40 +0530

    Enabled dashboard for all roles

--- src/App.js
+++ src/App.js
@@ -51,6 +51,7 @@ function App() {
                         <RoleProtectedRoute
                             allowedRoles={[
                                 "ADMIN",
+                                "DOCTOR",
                                 "RECEPTIONIST"
                             ]}
                         >
