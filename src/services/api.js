9132fa32055b48814721a1ddf0eab85767537eef
Ronit-bawankule <ronitbawankule20@gmail.com>
2026-05-27 12:26:45 +0530

    Fixed api export properly

--- src/services/api.js
+++ src/services/api.js
@@ -3,42 +3,15 @@ import axios from "axios";
 const BASE_URL =
 "https://hospital-management-backend-u64d.onrender.com";
 
-export const getPatients = async () => {
+export const getDoctors = async () => {
 
     return await axios.get(
-        `${BASE_URL}/patients`
+        `${BASE_URL}/doctors`
     );
-};
+};import axios from "axios";
 
-export const addPatient = async (
-    patient
-) => {
-
-    return await axios.post(
-        `${BASE_URL}/patients`,
-        patient
-    );
-};
-
-export const updatePatient = async (
-    id,
-    patient
-) => {
-
-    return await axios.put(
-        `${BASE_URL}/patients/${id}`,
-        patient
-    );
-};
-
-export const deletePatient = async (
-    id
-) => {
-
-    return await axios.delete(
-        `${BASE_URL}/patients/${id}`
-    );
-};
+const BASE_URL =
+"https://hospital-management-backend-u64d.onrender.com";
 
 export const getDoctors = async () => {
 
