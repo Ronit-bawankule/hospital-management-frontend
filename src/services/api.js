5c0b42eaae3dd35b12ed228ec5b5fde1e62fb89b
Ronit Bawankule <150640545+Ronit-bawankule@users.noreply.github.com>
2026-05-27 12:36:24 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -8,14 +8,4 @@ export const getDoctors = async () => {
     return await axios.get(
         `${BASE_URL}/doctors`
     );
-};import axios from "axios";
-
-const BASE_URL =
-"https://hospital-management-backend-u64d.onrender.com";
-
-export const getDoctors = async () => {
-
-    return await axios.get(
-        `${BASE_URL}/doctors`
-    );
 };
